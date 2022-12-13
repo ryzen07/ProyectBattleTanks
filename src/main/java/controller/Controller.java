e783ca60c98662dbb977c2b287feab337c738b7a
Emiliano <emilianoperez65@gmail.com>
2022-12-12 21:23:09 -0300

    Se agrega clase Controller

---
+++ src/main/java/controller/Controller.java
@@ -0,0 +1,39 @@
+package controller;
+
+import model.Board;
+import model.Bullet;
+import model.Direction;
+import model.Position;
+import model.Tank;
+
+public class Controller {
+	Position position;
+	Board map;
+	Tank tank;
+	Bullet bullet;
+
+	public Controller() {
+		createElements();
+		addEntities();
+	}
+
+	public void run() throws InterruptedException {
+		System.out.println(map);
+		while (true) {
+			update();
+		}
+	}
+
+	public void update() throws InterruptedException {
+		map.update();
+	}
+
+	public void createElements() {
+		map = new Board();
+		tank = new Tank(Direction.UP, 5, 5);
+	}
+
+	public void addEntities() {
+		map.addEntity(tank);
+	}
+}
