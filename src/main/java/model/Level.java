b84eafa69533bffe84dbfb2e512ea03606d54814
Emiliano <emilianoperez65@gmail.com>
2023-01-16 15:28:59 -0300

    se modifica level

--- src/main/java/model/Level.java
+++ src/main/java/model/Level.java
@@ -3,12 +3,7 @@ package model;
 import model.directions.Up;
 
 public class Level {
-
-<<<<<<< HEAD
 	Movable tank = new Tank(7, 7, new Up());
-=======
-	Tank tank = new Tank(7, 7, new Up());
->>>>>>> 4c9231a (Se modifica tostring de la clase board(para que tenga correlacion el movimiento del tanque y como lo vemos en pantalla). Se modifican los metodos de la clase  movable que trabajan con los limites del mapa para que el tanque llegue a los limites y no pueda pasarse. Se modifica la posicion del metodo checkNextPositionToMove de la linea 42 a la 43 en la clase Tank. Se crea clase PlayerTwo y se agrega un segundo tanque. Se modifica clase Level para instanciar y agregar el segundo tanque)
 	Tank tanktwo = new Tank(14, 14, new Up());
 	Board map = new Board();
 
