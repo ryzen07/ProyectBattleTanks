12b1b30bc92497f480efe150732853982765c46a
Emiliano <emilianoperez65@gmail.com>
2023-04-08 13:37:04 -0300

    se agrega clase player

---
+++ src/main/java/controller/player/Player.java
@@ -0,0 +1,12 @@
+package controller.player;
+
+public abstract class Player {
+
+	public abstract void Applymovement();
+
+	public void applyMovement() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
